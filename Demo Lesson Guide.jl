106dfc53a671f9f0aa51789e568e748632556e26
David Tan Creti <paradocs.home@gmail.com>
2021-02-19 20:56:01 -0600

    Create Demo Lesson Guide.jl

---
+++ Demo Lesson Guide.jl
@@ -0,0 +1,93 @@
+### A Pluto.jl notebook ###
+# v0.12.20
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ e4d3550a-72dd-11eb-06bc-fb27a9d9cd72
+md"""##### SUBJECT:ClassId;YourId;NotebookID;MM/DD/YYYY
+ClassExampleMissSmith123;
+MyIdDavid123;
+NotbookIdChemistryGrade6;
+02/15/2021
+"""
+
+# ╔═╡ 61919858-72df-11eb-1745-67ab5e72244b
+md"""##### EMAIL:Teacher@email.com;mySelf@email.com;myFriend@email.com
+mySelf@email.com;
+myTeacher@email.com;
+myFriend@email.com
+"""
+
+# ╔═╡ 63a1b84a-7324-11eb-0682-1fb702507e32
+md"""#### $\mathbb Notebook ~~~ \mathbb Name ~~~ Version:12/11/2020$
+source:[Web GitHub]()
+"""
+
+# ╔═╡ 7fe2d7b6-72e1-11eb-1a1a-afdd77020baa
+md"""##### SAVE/SEND:
+1) Your work will be lost if not sent by e-mail.
+2) Send youself copies as you work. for backups.  
+3) Answers must be in md-cells for the teacher to see.
+4) Click top right △Export and then △Notebook file.
+5) Open the new tab, Ctrl A to select all, Ctrl c to copy.
+6) Open a new e-mail and Ctrl v paste in to the body.
+7) Select and past the list of subject lines.
+8) Select and past the first or all of the e-mail lines.
+9) Check thay you have copied correctly and SEND.
+"""
+
+# ╔═╡ a30fd548-72f0-11eb-0180-f1902eece57a
+md"""##### NEW NOTEBOOK: (proposed -- work in progress)
+1) Start an empty web Pluto Notebook or use one already open.
+2) Open a web page or e-mail with the new notebook code.
+3) Slelet all code with Ctrl a (or mouse) and copy with Ctrl c.
+4) Click at the start of the top address bar and delete all of it.
+5) With Ctrl v paste the new code.
+6) Click the botton to open the new notebook.
+"""
+
+# ╔═╡ 7dd2db00-7300-11eb-20a9-ef06117f61e2
+md"""##### HELP/HINT$~~$ EMAIL SYSTEM
+If your teacher or assistents are available to scan for hint requests:
+
+write your request in a cell as $~~~$ hint/yyyy/mm/dd $~~~$ and send.
+
+
+Instead of the date, you may have a token $~~~$ hint/token $~~~$ to use.
+!!! start
+"""
+
+# ╔═╡ 56a0cb00-7300-11eb-0355-894bc9fb72da
+
+
+# ╔═╡ c3b85594-72f5-11eb-3717-a1c384543220
+md"""
+###### Problem(1)
+
+Write an expresstion to add 3 plus 5 plus one-half,
+
+Click the Answer 1 cell and type in your answer.
+
+"""
+
+# ╔═╡ 9b1f5900-72f5-11eb-24ac-2b31323430fd
+a = 5 + .5
+
+
+# ╔═╡ 8f48eaa0-72f6-11eb-16b9-1ff99b6c6a9e
+md"""###### Answer(1) =
+a = 5.5 
+"""
+
+# ╔═╡ Cell order:
+# ╟─e4d3550a-72dd-11eb-06bc-fb27a9d9cd72
+# ╟─61919858-72df-11eb-1745-67ab5e72244b
+# ╠═63a1b84a-7324-11eb-0682-1fb702507e32
+# ╟─7fe2d7b6-72e1-11eb-1a1a-afdd77020baa
+# ╠═a30fd548-72f0-11eb-0180-f1902eece57a
+# ╠═7dd2db00-7300-11eb-20a9-ef06117f61e2
+# ╠═56a0cb00-7300-11eb-0355-894bc9fb72da
+# ╠═c3b85594-72f5-11eb-3717-a1c384543220
+# ╠═9b1f5900-72f5-11eb-24ac-2b31323430fd
+# ╠═8f48eaa0-72f6-11eb-16b9-1ff99b6c6a9e
