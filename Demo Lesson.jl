2ac11530ff7d4c81b81bebffa391cae4001ab154
David Tan Creti <paradocs.home@gmail.com>
2021-03-10 21:21:25 -0600

    Update Demo Lesson.jl

--- Demo Lesson.jl
+++ Demo Lesson.jl
@@ -43,21 +43,6 @@ https://github.com/paradocs/pluto-community/blob/main/Demo%20Lesson.jl
 Copied from e-mail sent to student.
 """
 
-# ╔═╡ 592e7fb2-8182-11eb-2066-376bc42c6d1e
-x = [0, 2, 3, 5]     #y = rand(4, 2)
-
-# ╔═╡ 98f1eb34-8187-11eb-05b7-c79cd8da3a7f
-y = [2  5;  1  5; 4  1 ; 5  3 ]    
-
-# ╔═╡ a81ca4de-8182-11eb-2a7a-03d27b8d1e46
-plot(x, y , title = "Two Lines", label = ["L 1" "L 2"], lw = 3)
-
-# ╔═╡ c6021c2c-818a-11eb-0a03-e529ec84eeba
-scatter(x,y)
-
-# ╔═╡ 71f358ae-818b-11eb-12e5-57c5287ff306
-histogram(x, y)
-
 # ╔═╡ 7fe2d7b6-72e1-11eb-1a1a-afdd77020baa
 md"""##### SAVE/SEND:
 1) Your work will be lost if not sent by e-mail.
@@ -126,21 +111,38 @@ html"""
 <div style="width:680px;height:300px;border: 1px solid black;"><div style="position:relative;z-index:10;height:40px;padding-left:4px;width:150px;"><a style="text-decoration:none;color:#CCC;font-size:20px;font-family:Dosis;" href="https://ziteboard.com" target="_blank">Zoom & Move</a></div><iframe seamless="seamless" style="position:relative;width: 100%; height: 100%;top:-40px;" src="https://view.ziteboard.com/shared/07449027541615" frameborder="0" allowfullscreen></iframe></div>
 """
 
+# ╔═╡ 008d8264-8217-11eb-0ff7-e197e911adb9
+md"""
+### Here is some example plots:
+"""
+
+# ╔═╡ 592e7fb2-8182-11eb-2066-376bc42c6d1e
+x = [1, 2, 3, 4] 
+
+# ╔═╡ 98f1eb34-8187-11eb-05b7-c79cd8da3a7f
+y = [2  5;  1  5; 4  1 ; 5  3 ]    
+
+# ╔═╡ a81ca4de-8182-11eb-2a7a-03d27b8d1e46
+plot(x, y , title = "Two Lines", label = ["L 1" "L 2"], lw = 3)
+
+# ╔═╡ c6021c2c-818a-11eb-0a03-e529ec84eeba
+scatter(x,y)
+
 # ╔═╡ Cell order:
 # ╠═e4d3550a-72dd-11eb-06bc-fb27a9d9cd72
 # ╠═61919858-72df-11eb-1745-67ab5e72244b
-# ╠═63a1b84a-7324-11eb-0682-1fb702507e32
+# ╟─63a1b84a-7324-11eb-0682-1fb702507e32
 # ╠═f6cec062-8180-11eb-3722-b70b0d2873ac
-# ╠═592e7fb2-8182-11eb-2066-376bc42c6d1e
-# ╠═98f1eb34-8187-11eb-05b7-c79cd8da3a7f
-# ╠═a81ca4de-8182-11eb-2a7a-03d27b8d1e46
-# ╠═c6021c2c-818a-11eb-0a03-e529ec84eeba
-# ╠═71f358ae-818b-11eb-12e5-57c5287ff306
 # ╟─7fe2d7b6-72e1-11eb-1a1a-afdd77020baa
 # ╟─a30fd548-72f0-11eb-0180-f1902eece57a
 # ╟─7dd2db00-7300-11eb-20a9-ef06117f61e2
-# ╠═c3b85594-72f5-11eb-3717-a1c384543220
+# ╟─c3b85594-72f5-11eb-3717-a1c384543220
 # ╠═9b1f5900-72f5-11eb-24ac-2b31323430fd
 # ╠═aae018b4-74af-11eb-2bd7-f58e6e737e0b
 # ╟─ff44b926-78d5-11eb-3ea6-e16e11b73242
 # ╟─5844acaa-78d3-11eb-1582-717d7015401c
+# ╠═008d8264-8217-11eb-0ff7-e197e911adb9
+# ╠═592e7fb2-8182-11eb-2066-376bc42c6d1e
+# ╠═98f1eb34-8187-11eb-05b7-c79cd8da3a7f
+# ╠═a81ca4de-8182-11eb-2a7a-03d27b8d1e46
+# ╠═c6021c2c-818a-11eb-0a03-e529ec84eeba
