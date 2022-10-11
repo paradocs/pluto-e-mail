612c4f7b6d06077a72aacd05e26adc96938ee889
David Tan Creti <paradocs.home@gmail.com>
2022-10-11 01:31:16 -0500

    Create Blank.jl
    Blank template to start learning Julia and sending Pluto notebooks by email.

---
+++ Blank.jl
@@ -0,0 +1,66 @@
+### A Pluto.jl notebook ###
+# v0.19.12
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 61919858-72df-11eb-1745-67ab5e72244b
+md"""
+##### TO EMAIL YOUR PROGRAM:
+###### Type *EMAIL ADDRESSES* in front of the$~~$;$~~$ lines.$~~~$Copy and Paste for$~~~$ TO.
+;
+;
+;
+;
+;
+"""
+
+# ╔═╡ 582ff494-098a-4c86-b85b-025b47a8b0a1
+md"""###### Type your Id, class, and date in front of the$~~$;$~~$lines.$~~~$ Copy and Paste for$~~~$ Subject.
+;
+;
+;
+"""
+
+# ╔═╡ 592e7fb2-8182-11eb-2066-376bc42c6d1e
+md"""
+###### Click the triangle-circle and Notebook file.$~~$Copy and Paste into the email body.
+
+
+
+"""
+
+# ╔═╡ 98f1eb34-8187-11eb-05b7-c79cd8da3a7f
+
+
+# ╔═╡ a81ca4de-8182-11eb-2a7a-03d27b8d1e46
+
+
+# ╔═╡ c6021c2c-818a-11eb-0a03-e529ec84eeba
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.8.2"
+manifest_format = "2.0"
+project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╠═61919858-72df-11eb-1745-67ab5e72244b
+# ╠═582ff494-098a-4c86-b85b-025b47a8b0a1
+# ╟─592e7fb2-8182-11eb-2066-376bc42c6d1e
+# ╟─98f1eb34-8187-11eb-05b7-c79cd8da3a7f
+# ╟─a81ca4de-8182-11eb-2a7a-03d27b8d1e46
+# ╟─c6021c2c-818a-11eb-0a03-e529ec84eeba
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
