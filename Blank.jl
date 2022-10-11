375bedd4ce90fb5e1b8b182955d102223b7b96d4
David Tan Creti <paradocs.home@gmail.com>
2022-10-11 17:54:34 -0500

    Update Blank.jl
    spelling correction

--- Blank.jl
+++ Blank.jl
@@ -7,7 +7,7 @@ using InteractiveUtils
 # ╔═╡ 61919858-72df-11eb-1745-67ab5e72244b
 md"""
 ##### TO EMAIL YOUR PROGRAM:
-###### Type *EMAIL ADDRESSES* in front of the$~~$;$~~$ lines.$~~~$Copy and Paste for$~~~$ TO.
+###### Type *EMAIL ADDRESSES* in front of the$~~$;$~~$ lines.$~~~$Copy and Paste into$~~~$ TO.
 ;
 ;
 ;
@@ -16,7 +16,8 @@ md"""
 """
 
 # ╔═╡ 582ff494-098a-4c86-b85b-025b47a8b0a1
-md"""###### Type your Id, class, and date in front of the$~~$;$~~$lines.$~~~$ Copy and Paste for$~~~$ Subject.
+md"""###### Type *Class*, *Date*, *ID*, and *Name* in front of the$~~$;$~~$lines.$~~$Copy and Paste into$~~$ SUBJECT.
+;
 ;
 ;
 ;
@@ -34,7 +35,14 @@ md"""
 
 
 # ╔═╡ a81ca4de-8182-11eb-2a7a-03d27b8d1e46
+md"""
+##### Help:
+Use the 1st cell to make it easy to save and send the program to yourself and others.
 
+Use the 2nd cell for an email subject line.  Then you can search to find your versions.
+
+Paste the program into an email body frequently so you do not lose your work.
+"""
 
 # ╔═╡ c6021c2c-818a-11eb-0a03-e529ec84eeba
 
@@ -60,7 +68,7 @@ project_hash = "da39a3ee5e6b4b0d3255bfef95601890afd80709"
 # ╠═582ff494-098a-4c86-b85b-025b47a8b0a1
 # ╟─592e7fb2-8182-11eb-2066-376bc42c6d1e
 # ╟─98f1eb34-8187-11eb-05b7-c79cd8da3a7f
-# ╟─a81ca4de-8182-11eb-2a7a-03d27b8d1e46
+# ╠═a81ca4de-8182-11eb-2a7a-03d27b8d1e46
 # ╟─c6021c2c-818a-11eb-0a03-e529ec84eeba
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
